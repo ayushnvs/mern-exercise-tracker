8670fa3f9b216a8bbc7104018e369965693b1a1e
ayushnvs <ayush.mishra.ald@gmail.com>
2023-05-14 18:36:41 +0530

    creating UI

---
+++ src/components/create-user.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+
+export default class CreateUser extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Create User component!</p>
+            </div>
+        )
+    }
+}
